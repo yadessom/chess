2e592ac1abd3e3f70fee180750e61e168b788dca
yadess <90118673+yadessom@users.noreply.github.com>
2022-01-08 02:58:08 +0900

    + gnb

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
-    
+    $('.gnb li').mouseenter(function(){
+        $('.depth2_back').stop().slideDown();
+        $('.depth2').stop().slideDown();
+    });
+    $('.gnb').mouseleave(function(){
+        $('.depth2_back').stop().slideUp();
+        $('.depth2').stop().slideUp();
+    });
 
 
 

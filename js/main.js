e1ac360539d4a6c5a22052440ffab31a5e9db045
yadess <90118673+yadessom@users.noreply.github.com>
2022-01-08 11:41:39 +0900

    desktop page fin

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(function(){
 
     $('.gnb li').mouseenter(function(){
-        $('.depth2_back').stop().slideDown();
-        $('.depth2').stop().slideDown();
+        $('.depth2_back').stop().slideDown(250);
+        $('.depth2').stop().slideDown(300);
     });
     $('.gnb').mouseleave(function(){
-        $('.depth2_back').stop().slideUp();
-        $('.depth2').stop().slideUp();
+        $('.depth2_back').stop().slideUp(250);
+        $('.depth2').stop().slideUp(300);
     });
 
 

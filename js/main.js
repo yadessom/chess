9d4749d551baeb7148126b7fc6f7ae7ea553bdb7
yadess <90118673+yadessom@users.noreply.github.com>
2022-01-08 16:26:31 +0900

    + sec 07

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ $(function(){
         $('.depth2_back').stop().slideUp(250);
         $('.depth2').stop().slideUp(300);
     });
+    $('.sec_03_more').click(function(){
+        $('.sec_03_more').toggleClass('on');
+        $('.sec_03_news').stop().slideToggle();
+    });
+    $('.sec_04_more').click(function(){
+        $('.sec_04_more').toggleClass('on');
+        $('.sec_04_game').stop().slideToggle();
+    });
 
 
 
